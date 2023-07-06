51be8626f1581ab83535f277ff1f7a03e5f46981
arota18 <104698199+arota18@users.noreply.github.com>
2023-07-06 21:33:06 +0200

    Initial Commit

---
+++ LoneWolfAppSwift/LoneWolfAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoneWolfAppSwiftApp.swift
+//  LoneWolfAppSwift
+//
+//  Created by Andrea Rota on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LoneWolfAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
